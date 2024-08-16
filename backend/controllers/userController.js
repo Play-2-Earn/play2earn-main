c0821d3044b5a24b78ccb62ddb53fe4b18a3a4db
Mit Prajapati <113085819+mit544@users.noreply.github.com>
2024-08-16 15:51:58 +0100

    Update userController.js
    added functionality to let user to put username, and the username is being checked that whether it is unique or not.

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,172 +1,163 @@
+const express = require('express');
 const UserModel = require('../models/User');
 // mit prajapati (auth)
-const mongoose = require('mongoose');
 const nodemailer = require("nodemailer");
 const { request } = require("http");
 require("dotenv").config();
 const crypto = require("crypto");
 
-// Get a user's profile
-exports.getUserProfile = async (req, res) => {
-    try {
-        const user = await UserModel.findById(req.params.id);
-        if (!user) {
-            return res.status(404).send();
-        }
-        res.send(user);
-    } catch (error) {
-        res.status(500).send(error);
-    }
-};
 
-// Update a user's profile
-exports.updateUserProfile = async (req, res) => {
-    const updates = Object.keys(req.body);
-    const allowedUpdates = ['username', 'email', 'password', 'profile_pic_url', 'tasks_completed', 'total_rewards'];
-    const isValidOperation = updates.every((update) => allowedUpdates.includes(update));
+const { getUserProfile, updateUserProfile, deleteUser, getAllUsers, sign_up, log_in, requestUserPasswordReset, passwordSet } = require('../controllers/userController');
+const { error } = require('console');
 
-    if (!isValidOperation) {
-        return res.status(400).send({ error: 'Invalid updates!' });
-    }
+const router = express.Router();
+// router.use(express.json());
 
-    try {
-        const user = await UserModel.findById(req.params.id);
+router.get('/:id', getUserProfile);
+router.patch('/:id', updateUserProfile);
+router.delete('/:id', deleteUser);
+router.get('/', getAllUsers);
 
-        if (!user) {
-            return res.status(404).send();
-        }
+// mit prajapati (authentication system)
 
-        updates.forEach((update) => {
-            user[update] = req.body[update];
+// code for generating the reference code...
+function generateReferralCode(firstName, lastName) {
+    const data = `${firstName}-${crypto.randomInt(100000, 999999)}-${lastName}`;
+    const hash = crypto.createHash('sha256').update(data).digest('hex');
+
+    // Truncate the hash to the first 8 characters (you can choose a different length)
+    const referralCode = hash.substring(0, 10);
+    return referralCode;
+}
+
+
+router.post("/sign_up", (request, response) => {
+    const { firstName, lastName, email, username, password, refBy } = request.body;
+    // this code if for the functionality to generate the usename by itself with the firstname of a user
+    // const username = firstName + crypto.randomInt(100000, 999999);
+
+    // Checking that if a user with the given email already exists
+    UserModel.findOne({ email: email })
+        .then(user => {
+            if (user) {
+                // If the user is found, send a response indicating the email is already in use
+                return response.status(400).json({ error: "Email already exists" });
+
+                //  checking that the username is arleady exists
+                // UserModel.findOne({username : username})
+            }
+
+            else {
+                return UserModel.findOne({ username: username })
+            }
+        })
+        .then(ifSimilarUsername => {
+            if (ifSimilarUsername) {
+                response.status(401).json({ error: "Username is already taken! Enter unique username." })
+            }
+                // Generate the referral code and create the new user
+                const userRefNum = generateReferralCode(firstName, lastName);
+
+               return UserModel.create({ firstName, lastName, username, email, password, userRefNum, refBy })
+                   
+        })
+        .then(newUser =>{
+            return response.status(201).json({ message: "User created successfully", user: newUser });
+        })
+        .catch(err => {
+            if (response.headersSent) {
+                // If headers are already sent, do not attempt to send another response
+                console.error("Headers already sent. Cannot send another response.");
+                return;
+            }
+            
+            if (error.code === "11000") {
+                response.status(300).json({error: "Either the username or the email address already exist"})
+                
+            }
+            console.error("Error finding user:", err);
+            response.status(500).json({ error: "Error finding user" });
         });
-        await user.save();
-        res.send(user);
-    } catch (error) {
-        res.status(400).send(error);
-    }
-};
+});
 
-// Delete a user
-exports.deleteUser = async (req, res) => {
-    try {
-        const user = await UserModel.findByIdAndDelete(req.params.id);
+router.post("/log_in", (request, response) => {
+    const { email, password } = request.body;
+    UserModel.findOne({ email: email })
+        .then(user => {
+            if (user) {
+                if (user.password === password) {
+                    response.json("success");
+                } else {
+                    response.json("The credentials are incorrect");
+                }
+            } else {
+                response.json("User doesn't exist");
+            }
+        });
+});
 
-        if (!user) {
-            return res.status(404).send();
-        }
 
-        res.send(user);
-    } catch (error) {
-        res.status(500).send(error);
-    }
-};
 
-// List all users
-exports.getAllUsers = async (req, res) => {
-    try {
-        const users = await UserModel.find();
-        res.json(users);
-    } catch (error) {
-        res.status(500).json({ error: error.message });
-    }
-};
+router.post("/requestUserPasswordReset", (request, response) => {
+    const { email } = request.body;
+    UserModel.findOne({ email: email })
+        .then(user => {
+            if (user) {
+                const otp = crypto.randomInt(100000, 999999);
+                user.otp = otp;
+                user.otpExpires = Date.now() + 3600000; // 1 hour
 
-// authentication (Mit Prajapati)
+                user.save();
+                const transporter = nodemailer.createTransport({
+                    service: "gmail",
+                    auth: {
+                        user: "playearnai.noreply@gmail.com",
+                        pass: "htcspcsiaiykuklx"
+                    }
+                });
 
-// app.use(cors({
-//     origin: 'http://localhost:5173'
-// }));
-// const app = express();
-// app.use(express.json());
+                const mailOptions = {
+                    to: user.email,
+                    from: process.env.EMAIL,
+                    subject: "Password Reset",
+                    text: `You are receiving this because you (or someone else) have requested the reset of the password for your account.\n\n
+                    Here is the code which you need to enter:${otp}\n\n
+                    This code will be expire in one hour, and please make sure that you do not share with anyone else for security reason.
+                    If you did not request this, please ignore this email and your password will remain unchanged.\n`
+                };
 
-// mongoose.connect("mongodb://127.0.0.1:27017/Users_1214522145522121");
+                transporter.sendMail(mailOptions, (err, res) => {
+                    if (err) {
+                        console.error("There was an error: ", err);
+                        return response.status(500).json({ error: "Failed to send email" });
+                    } else {
+                        return response.status(200).json({ message: "Password reset email sent" });
+                    }
+                });
+            } else {
+                return response.status(404).json({ error: "User is not found" });
+            }
+        })
+        .catch(err => response.status(500).json({ error: err.message }));
+});
 
-// exports.sign_up = async (request, response) => {
-//     UserModel.create(request.body)
-//     user.save()
-//         .then(users => response.json(users))
-//         .catch(err => response.json(err));
-// };
+router.post("/passwordSet", (request, response) => {
+    const { otp, newPassword } = request.body
+    UserModel.findOne({ otp: otp })
+        .then(user => {
+            if (otp) {
+                user.password = newPassword;
+                user.save()
+                response.json("correct");
+                response.json(user.password)
+            }
+            else {
+                response.json("OTP is incorrect. Try again!")
+            }
+        })
+        .catch(err => {
+            response.status(500).json({ message: "Error finding user.", error: err });
+        });
+})
 
-// exports.log_in = (request, response) => {
-//     const { email, password } = request.body;
-//     UserModel.findOne({ email: email })
-//         .then(user => {
-//             if (user) {
-//                 if (user.password === password) {
-//                     response.json("success");
-//                 } else {
-//                     response.json("The credentials are incorrect");
-//                 }
-//             } else {
-//                 response.json("User doesn't exist");
-//             }
-//         });
-// };
-
-// const transporter = nodemailer.createTransport({
-//     service: "gmail",
-//     auth: {
-//         user: process.env.EMAIL,
-//         pass: process.env.EMAIL_PASSWORD
-//     }
-// });
-
-// exports.requestUserPasswordReset = (request, response) => {
-//     const { email } = request.body;
-//     UserModel.findOne({ email: email })
-//         .then(user => {
-//             if (user) {
-//                 const otp = crypto.randomInt(100000, 999999);
-//                 user.otp = otp;
-//                 user.otpExpires = Date.now() + 3600000; // 1 hour
-
-//                 user.save();
-
-//                 const mailOptions = {
-//                     to: user.email,
-//                     from: process.env.EMAIL,
-//                     subject: "Password Reset",
-//                     text: `You are receiving this because you (or someone else) have requested the reset of the password for your account.\n\n
-//                     Here is the code which you need to enter:${otp}\n\n
-//                     This code will be expire in one hour, and please make sure that you do not share with anyone else for security reason.
-//                     If you did not request this, please ignore this email and your password will remain unchanged.\n`
-//                 };
-
-//                 transporter.sendMail(mailOptions, (err, res) => {
-//                     if (err) {
-//                         console.error("There was an error: ", err);
-//                         return response.status(500).json({ error: "Failed to send email" });
-//                     } else {
-//                         return response.status(200).json({ message: "Password reset email sent" });
-//                     }
-//                 });
-//             } else {
-//                 return response.status(404).json({ error: "User is not found" });
-//             }
-//         })
-//         .catch(err => response.status(500).json({ error: err.message }));
-// };
-
-// exports.passwordSet = (request, response) => {
-//     const { otp, newPassword } = request.body
-//     UserModel.findOne({ otp: otp })
-//         .then(user => {
-//             if (otp) {
-//                 user.password = newPassword;
-//                 user.save()
-//                 response.json("correct");
-//                 response.json(user.password)
-//             }
-//             else {
-//                 response.json("OTP is incorrect. Try again!")
-//             }
-//         })
-//         .catch(err => {
-//             response.status(500).json({ message: "Error finding user.", error: err });
-//         });
-// }
-
-// app.listen(3001, () => {
-//     console.log("Server is running on port 3001");
-// });
+module.exports = router;
