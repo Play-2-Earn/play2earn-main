e494a9bf4a919872dd721aa561a8feb856b536bb
Mit Prajapati <113085819+mit544@users.noreply.github.com>
2024-08-16 15:52:51 +0100

    Update Signup.jsx

--- src/components/Signup.jsx
+++ src/components/Signup.jsx
@@ -9,6 +9,7 @@ const SignUpPopup = ({ isOpen, onClose, innLogInlink }) => {
   const [firstName, setFirstName] = useState('');
   const [lastName, setLastName] = useState('');
   const [email, setEmail] = useState('');
+  const [username, setUsername] = useState('');
   const [password, setPassword] = useState('');
   const [refBy, setRef_num] = useState('')
   // const navigate = useNavigate()
@@ -20,7 +21,7 @@ const SignUpPopup = ({ isOpen, onClose, innLogInlink }) => {
 
     try {
         const response = await axios.post('http://localhost:5002/api/users/sign_up', {
-            firstName, lastName, email, password, refBy
+            firstName, lastName, email, username, password, refBy
         });
 
         console.log(response);
@@ -91,6 +92,23 @@ const SignUpPopup = ({ isOpen, onClose, innLogInlink }) => {
               type="email"
               value={email}
               onChange={(e) => setEmail(e.target.value)}
+              className="w-full px-3 py-2 border rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-500"
+              required style={{
+                width: 'calc(100% - 0.5rem)',
+                backgroundColor: '#d5dbdb30',
+                borderBottom: '1px solid rgba(193, 199, 205, 1)'
+              }}
+            />
+          </div>
+
+          <div className="mb-4">
+            <label className="block text-black text-sm font mb-2" htmlFor="username">Username</label>
+            <input
+              placeholder="example@gmail.com"
+              id="username"
+              type="text"
+              value={username}
+              onChange={(e) => setUsername(e.target.value)}
               className="w-full px-3 py-2 border rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-500"
               required style={{
                 width: 'calc(100% - 0.5rem)',
