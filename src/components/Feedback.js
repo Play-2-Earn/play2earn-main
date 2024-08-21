21fe4e7094a4f7804129ff84950458c4568a98ee
Qadir Killedar <qadirk2003@gmail.com>
2024-08-21 21:59:56 +0530

    removed audio changes

--- src/components/Feedback.js
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-
-const Feedback = ({ message, isError }) => {
-  return <div className={`feedback ${isError ? 'error' : ''}`}>{message}</div>;
-};
-
-export default Feedback;
