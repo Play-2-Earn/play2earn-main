35f0398daf4a7ffb676dd4860b5c2667e353d26e
Reene <67369326+Reene444@users.noreply.github.com>
2024-09-10 13:54:20 +0100

    fixed bug for the icons display on mobile

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -378,6 +378,8 @@ const Home = () => {
           <h3 className="text-2xl md:text-3xl font-bold text-center mb-8 md:mb-12">
             Trusted by Brands You Know
           </h3>
+
+          
           {/*fixed bug for icons*/}
           <div className="grid grid-cols-3 md:grid-cols-3 gap-4 md:gap-8 justify-items-center">
             {partnerLogos.map((logo, index) => (
